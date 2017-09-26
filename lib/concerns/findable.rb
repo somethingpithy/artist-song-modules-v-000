c88afa3f29dce76646f95b54ac1066d206213e04
Carlota Bindner <cev.bindner@gmail.com>
2017-09-26 03:15:01 +0000

    automatically backed up by learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,8 +1,8 @@
 module Findable
   module ClassMethods
     def self.find_by_name(name)
-      @@artists.detect{|a| a.name == name}
-    end
+      self.all.detect{|a| a.name == name}
+    end  
   end
 
 end
