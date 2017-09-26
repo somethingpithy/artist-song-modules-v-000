5dae77cee947c49aaf3c51aa2527c0af4d23e5da
Carlota Bindner <cev.bindner@gmail.com>
2017-09-26 03:00:37 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,9 @@
 require 'pry'
 
 class Song
+
+  extend Memorable::ClassMethods
+
   attr_accessor :name
   attr_reader :artist
 
@@ -18,19 +21,20 @@ class Song
     @artist = artist
   end
 
-  def self.reset_all
-    @@songs.clear
-  end
+  #def self.reset_all
+  #  @@songs.clear
+  #end
 
   def to_param
     name.downcase.gsub(' ', '-')
   end
 
-  def self.count
-    self.all.count
-  end
+  #def self.count
+  #  self.all.count
+  #end
 
   def self.all
     @@songs
   end
+
 end
